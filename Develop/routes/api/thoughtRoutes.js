0754589fdb782d1719b88f7252d823080d16ca85
CoulsonP <coulsonp@icloud.com>
2023-02-28 11:24:23 -0600

    added thought routes

--- Develop/routes/api/thoughtRoutes.js
+++ Develop/routes/api/thoughtRoutes.js
@@ -1,22 +1,23 @@
-// const router = require('express').Router();
-// const {
-//   getThought,
-//   getSingleThought,
-//   createThought,
-//   addReaction
-//   removeReaction
-//   updateThought,
-//   deleteThought,
-// } = require('../../controllers/thoughtController.js');
+const router = require('express').Router();
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  addReaction,
+  removeReaction,
+  updateThought,
+  deleteThought,
+} = require('../../controllers/thoughtController.js');
 
-// // /api/courses
-// router.route('/').get(getThought).post(createThought);
+// /api/courses
+router.route('/').get(getThought).post(createThought);
 
-// // /api/thoughts/:thoughtId
-// router
-//   .route('/:thoughtId')
-//   .get(getSingleThought)
-//   .put(updateThought)
-//   .delete(deleteThought);
-
-// module.exports = router;
+// /api/thoughts/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+router.route("/:thoughtId/reactions").post(addReaction)
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction)
+module.exports = router;
